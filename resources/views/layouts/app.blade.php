d062e1d851a299f1591a3fb9a6045797d9a9d4d4
Adjagarail <43035463+Adjagarail@users.noreply.github.com>
2018-12-21 12:28:51 +0000

    auth securisé par email

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,11 +40,11 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                            <li class="nav-item {{ currentRoute(route('login')) }}">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
+                                <li class="nav-item {{ currentRoute(route('register')) }}">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
